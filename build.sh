e41ed475e89760c11db8becbb3ec26c22290d988
Ingo Steinke <ingosteinke@gmx.de>
2024-02-13 12:41:02 +0100

    more verbose and fail-safe build process

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
-mkdir build/intellij-cute-pink-light-theme
+echo "build theme distribution $1 ..."
+mkdir -pv build/intellij-cute-pink-light-theme
 rm -rf build/intellij-cute-pink-light-theme/*
-mkdir build/intellij-cute-pink-light-theme/lib
-mkdir build/intellij-cute-pink-light-theme-$1
-cp -r src/main/resources/* build/intellij-cute-pink-light-theme-$1/
+mkdir -pv build/intellij-cute-pink-light-theme/lib
+mkdir -pv build/intellij-cute-pink-light-theme-$1
+cp -rv src/main/resources/* build/intellij-cute-pink-light-theme-$1/
 cd build
 cd intellij-cute-pink-light-theme-$1
-zip -r ../intellij-cute-pink-light-theme/lib/intellij-cute-pink-light-theme-$1.jar ./*
+zip -rv ../intellij-cute-pink-light-theme/lib/intellij-cute-pink-light-theme-$1.jar ./*
 cd ..
-zip -r distributions/intellij-cute-pink-light-theme-$1.zip.tmp intellij-cute-pink-light-theme
+mkdir -pv distributions
+zip -rv distributions/intellij-cute-pink-light-theme-$1.zip.tmp intellij-cute-pink-light-theme
 rm -f distributions/intellij-cute-pink-light-theme-$1.zip
 mv distributions/intellij-cute-pink-light-theme-$1.zip.tmp distributions/intellij-cute-pink-light-theme-$1.zip
+echo "done"
